5b92c3090c790a419d011f5a18777479826a070e
333524585 <333524585@C-4028YR1.tdsb.on.ca>
2016-09-14 12:54:27 -0400

    AboutMe file 1 uploading to GidHub

---
+++ ics3u/src/nur/AboutMe.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package nur;
+
+/**
+ * AboutMe.java
+ * @author Halima Nur
+ * Created September 13th 2016
+ */
+public class AboutMe {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Halima Nur");
+		System.out.println("Ms.K");
+		System.out.println("Runnymede C.I");
+ 
+	}
+
+}
