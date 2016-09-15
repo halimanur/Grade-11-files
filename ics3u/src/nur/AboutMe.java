3bc9889b153e0292f663e893c21547141ce2e53c
333524585 <333524585@C-4028YR1.tdsb.on.ca>
2016-09-15 13:38:47 -0400

    My class schedule

--- ics3u/src/nur/AboutMe.java
+++ ics3u/src/nur/AboutMe.java
@@ -16,8 +16,40 @@ public class AboutMe {
 	public static void main(String[] args) {
 		System.out.println("Halima Nur");
 		System.out.println("Ms.K");
-		System.out.println("Runnymede C.I");
+		System.out.println("Runnymede C.I\n");
+		
  
+		System.out.format("%-15s %15s %15s", "Periods", "Day1", "Day2\n\n");
+		System.out.format("%-15s %15s %15s", "Period 1", "PPL2/3/4OF-01", "PPL2/3/4OF-01\n");
+		System.out.format("%-15s %15s %15s", "8;45", "Kotsopoulos K", "Kotsopoulos K\n");
+		System.out.format("%-15s %15s %15s", "10;02","GYM2", "GYM2\n\n");
+
+		System.out.format("%-15s %15s %15s", "Period 2", "SCH3U1-01", "SCH3U1-01\n");
+	    System.out.format("%-15s %15s %15s", "10:07", "Fife C", "Fife C\n");
+	    System.out.format("%-15s %15s %15s", "11:24", "303" , "303\n\n");
+	    
+	    System.out.format("%-15s %15s %15s","Lunch","","\n");
+	    System.out.format("%-15s %15s %15s","11:24","","\n");
+	    System.out.format("%-15s %15s %15s","12:26","","\n\n");
+	    
+	    System.out.format("%-15s %15s %15s", "Period 3", "ICS3C/3U1-01", "ICS3C/3U1-01\n");
+	    System.out.format("%-15s %15s %15s", "12:26", "Kemp C", "Kemp C\n");
+	    System.out.format("%-15s %15s %15s", "1:43", "24", "24\n\n");
+	    
+	    System.out.format("%-15s %15s %15s", "Period 4", "ENG3U1-01", "ENG3U1-01\n");
+	    System.out.format("%-15s %15s %15s", "2:48", "Gibbons C", "Gibbons C\n");
+	    System.out.format("%-15s %15s %15s", "3:05", "101", "101\n");
+	    
+	    		
+	    
+	    
+	    
+	    	
+	    
+	    
+	    
+	    
+	    
 	}
 
 }
