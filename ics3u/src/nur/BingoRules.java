ed37e4c36e297517ac973cd4fa77eaec133e3294
333524585 <333524585@C-4028YR1.tdsb.on.ca>
2016-09-19 13:09:12 -0400

    The rules of Bingo

---
+++ ics3u/src/nur/BingoRules.java
@@ -0,0 +1,28 @@
+package nur;
+
+public class BingoRules {
+	/**
+	 * @param args
+	 */
+	 
+	public static void main(String[] args) {
+		System.out.format("Bingo Card\n");
+		System.out.format("\n");
+		System.out.format("1. The caller randomly pulls a numbered bingo ball.\n");
+		System.out.format("\n");
+		System.out.format("2. The number is placed on the bingo board and called out.\n");
+		System.out.format("\n");
+		System.out.format("3. Players look for the called number on their bingo card.\n");
+		System.out.format("\n");
+		System.out.format("4. If the number is located, it is marked off.\n");
+		System.out.format("\n");
+		System.out.format("5. Steps 1 to 4 are repeated until a player matches the BINGO pattern.\n");
+		System.out.format("\n");
+		System.out.format("6. The winning player yells BINGO.\n");
+		
+				
+				
+
+	}
+
+}
