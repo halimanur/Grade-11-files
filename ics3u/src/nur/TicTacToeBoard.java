a00659bf4352eb48e17822dc68ee94f2129cc3b1
333524585 <333524585@C-4028YR1.tdsb.on.ca>
2016-09-19 13:22:09 -0400

    My ticTactoe board

---
+++ ics3u/src/nur/TicTacToeBoard.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package nur;
+
+/**
+ * @author Halima Nur
+ * September 19th 2016
+ *TicTacToeBoard.java
+ */
+public class TicTacToeBoard {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.format("%10s %7s", "|", "|\n");
+		System.out.format("%10s %7s", "|", "|\n");
+		System.out.format("%10s %7s", "|", "|\n");
+		System.out.format("%5s", "-------------------------\n");
+		System.out.format("%10s %7s", "|", "|\n");
+		System.out.format("%10s %2s %4s", "|","x", "|\n");
+		System.out.format("%10s %7s", "|", "|\n");
+		System.out.format("%5s", "-------------------------\n");
+		System.out.format("%10s %7s", "|", "|\n");
+		System.out.format("%10s %7s", "|", "|\n");
+		System.out.format("%10s %7s", "|", "|\n");
+		
+		
+
+	}
+
+}
