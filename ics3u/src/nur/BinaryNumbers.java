a2bb47a97478b5cf5089a770407b3c3258b95be1
333524585 <333524585@C-4028YR1.tdsb.on.ca>
2016-09-20 13:31:56 -0400

    Decimal and Binary numbers

--- ics3u/src/nur/BinaryNumbers.java
+++ ics3u/src/nur/BinaryNumbers.java
@@ -16,9 +16,27 @@ public class BinaryNumbers {
 	public static void main(String[] args) {
 		System.out.format("%10s %5s %5s", "Decimal", "", "Binary\n");
 		System.out.format("%10s %5s %5s", "1", "", "1\n");
-		System.out.format("10s %5s %5s", "2", "10\n");
-		
-		
+		System.out.format("%10s %5s %5s", "2", "", "10\n");
+		System.out.format("%10s %5s %5s", "3", "", "11\n");
+		System.out.format("%10s %5s %5s", "4", "", "100\n");
+		System.out.format("%10s %5s %5s", "5", "", "101\n");
+		System.out.format("%10s %5s %5s", "6", "", "110\n");
+		System.out.format("%10s %5s %5s", "7", "", "111\n");
+		System.out.format("%10s %5s %5s", "8", "", "1000\n");
+		System.out.format("%10s %5s %5s", "9", "", "1001\n");
+	    System.out.format("%10s %5s %5s", "10", "", "1010\n");
+	    System.out.format("%10s %5s %5s", "11", "", "1011\n");
+	    System.out.format("%10s %5s %5s", "12", "", "1100\n");
+	    System.out.format("%10s %5s %5s", "13", "", "1101\n");
+	    System.out.format("%10s %5s %5s", "14", "", "1110\n");
+	    System.out.format("%10s %5s %5s", "15", "", "1111\n");
+	    System.out.format("%10s %5s %5s", "16", "", "10000\n");
+	    System.out.format("%10s %5s %5s", "17", "", "10001\n");
+	    System.out.format("%10s %5s %5s", "18", "", "10010\n");
+	    System.out.format("%10s %5s %5s", "19", "", "10011\n");
+	    System.out.format("%10s %5s %5s", "20", "", "10100\n");
+	    
+	    		
 				
 				
 				
