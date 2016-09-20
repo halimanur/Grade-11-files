9426a695683040093db5039df849688f4493ea07
333524585 <333524585@C-4028YR1.tdsb.on.ca>
2016-09-20 13:00:30 -0400

    My defintion vocabalary list

---
+++ ics3u/src/nur/BinaryNumbers.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package nur;
+
+/**
+ * @author Halima Nur
+ * Date: September 20th 2016
+ *
+ */
+public class BinaryNumbers {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.format("%10s %5s %5s", "Decimal", "", "Binary\n");
+		System.out.format("%10s %5s %5s", "1", "", "1\n");
+		System.out.format("10s %5s %5s", "2", "10\n");
+		
+		
+				
+				
+				
+				
+
+	}
+
+}
---
+++ ics3u/src/nur/JavaTerminology.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package nur;
+
+/**
+ * @author Halima Nur
+ * September 20th 2016
+ * 
+ */
+public class JavaTerminology {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.format("Vocabulary\n");
+		System.out.format("\n");
+		System.out.format("1. String: A set of characters.\n");
+		System.out.format("\n");
+		System.out.format("2. Execute: To run a program.\n");
+		System.out.format("\n");
+		System.out.format("3. Class: The description of an object.\n");
+		System.out.format("\n");
+		System.out.format("4. Package: A group of related classes. Also called a library.\n");
+		System.out.format("\n");
+		System.out.format("5. Statement: An instruction in a program.\n");
+		System.out.format("\n");
+		
+
+	}
+
+}
