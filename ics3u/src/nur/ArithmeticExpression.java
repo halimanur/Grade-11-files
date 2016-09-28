944198bdeebb20c275a141b9bac3f8c0563f3ef2
333524585 <333524585@C-4028YR1.tdsb.on.ca>
2016-09-28 12:58:29 -0400

    Arithmetic Expressions assignment

---
+++ ics3u/src/nur/ArithmeticExpression.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package nur;
+import java.util.Scanner;
+
+/**
+ * @author Halima Nur
+ * September 26th 2016
+ * Java.util.scanner;
+ * 
+ */
+public class ArithmeticExpression {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int num1;
+		System.out.println("Enter 5 numbers.");
+		num1= scan.nextInt();
+		
+		int num2;
+		num2= scan.nextInt();
+		
+		int num3;
+		num3= scan.nextInt();
+		
+		int num4;
+		num4= scan.nextInt();
+		
+		int num5;
+		num5= scan.nextInt();
+		System.out.println("The sum of all five numbers: " + (num1+num2+num3+num4+num5));
+		System.out.println("The result after subtracting the second number from the third is: " + (num2-num3));
+		System.out.println("The product of the first and fifth number is: " + (num1*num5));
+		System.out.println("The quotient of the fourth number divided by the second is: " + (num4/num2));
+		System.out.println("The remainder when dividing the fourth number by the second is: " + (num4%num2));
+		
+		
+		
+				
+				
+		
+			
+		
+		
+		
+		
+		
+	
+
+	
+
+
+	}
+
+}
--- ics3u/src/nur/BinaryNumbers.java
+++ ics3u/src/nur/BinaryNumbers.java
@@ -14,17 +14,17 @@ public class BinaryNumbers {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.format("%10s %5s %5s", "Decimal", "", "Binary\n");
-		System.out.format("%10s %5s %5s", "1", "", "1\n");
-		System.out.format("%10s %5s %5s", "2", "", "10\n");
-		System.out.format("%10s %5s %5s", "3", "", "11\n");
-		System.out.format("%10s %5s %5s", "4", "", "100\n");
-		System.out.format("%10s %5s %5s", "5", "", "101\n");
-		System.out.format("%10s %5s %5s", "6", "", "110\n");
-		System.out.format("%10s %5s %5s", "7", "", "111\n");
-		System.out.format("%10s %5s %5s", "8", "", "1000\n");
-		System.out.format("%10s %5s %5s", "9", "", "1001\n");
-	    System.out.format("%10s %5s %5s", "10", "", "1010\n");
+		System.out.format("%10s %5s %5s %5s %5s", "Decimal", "", "Binary", "", "Hexadecimal\n");
+		System.out.format("%10s %5s %5s %5s %5s", "1", "", "1", "", "1\n");
+		System.out.format("%10s %5s %5s %5s %5s", "2", "", "10", "", "2\n");
+		System.out.format("%10s %5s %5s %5s %5s", "3", "", "11", "", "3\n");
+		System.out.format("%10s %5s %5s %5s %5s", "4", "", "100", "", "4\n");
+		System.out.format("%10s %5s %5s %5s %5s", "5", "", "101", "", "5\n");
+		System.out.format("%10s %5s %5s %5s %5s", "6", "", "110", "", "6\n");
+		System.out.format("%10s %5s %5s %5s %5s", "7", "", "111", "", "7\n");
+		System.out.format("%10s %5s %5s %5s %5s", "8", "", "1000", "", "8\n");
+		System.out.format("%10s %5s %5s %5s %5s", "9", "", "1001", "", "9\n");
+	    System.out.format("%10s %5s %5s %5s %5s", "10", "", "1010", "", "A\n");
 	    System.out.format("%10s %5s %5s", "11", "", "1011\n");
 	    System.out.format("%10s %5s %5s", "12", "", "1100\n");
 	    System.out.format("%10s %5s %5s", "13", "", "1101\n");
