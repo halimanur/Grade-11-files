e07a613281a427f18feff51a05cbe76577299aad
333524585 <333524585@C-4028YR1.tdsb.on.ca>
2016-09-30 12:46:05 -0400

    Arithmetic Expressions

--- ics3u/src/nur/ArithmeticExpression.java
+++ ics3u/src/nur/ArithmeticExpression.java
@@ -33,11 +33,17 @@ public class ArithmeticExpression {
 		
 		int num5;
 		num5= scan.nextInt();
-		System.out.println("The sum of all five numbers: " + (num1+num2+num3+num4+num5));
+		System.out.println("The sum of all five numbers is: " + (num1+num2+num3+num4+num5));
 		System.out.println("The result after subtracting the second number from the third is: " + (num2-num3));
 		System.out.println("The product of the first and fifth number is: " + (num1*num5));
 		System.out.println("The quotient of the fourth number divided by the second is: " + (num4/num2));
 		System.out.println("The remainder when dividing the fourth number by the second is: " + (num4%num2));
+		System.out.println("The first number raised to the power of the third number is: " + Math.pow (num1,num3));
+		System.out.println("The square root of the fifth number is: " + Math.sqrt(num5));
+		
+		
+
+
 		
 		
 		
