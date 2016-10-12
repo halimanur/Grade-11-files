e415bc61a8ef98f10fb0ae52aaaffd8aef459743
333524585 <333524585@C-4028YR1.tdsb.on.ca>
2016-10-12 13:22:24 -0400

    Resturant order

---
+++ ics3u/src/nur/OrderApplication.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package nur;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+/**
+ * @author 333524585
+ *
+ */
+public class OrderApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner Scan = new Scanner(System.in);
+		DecimalFormat df= new DecimalFormat ("0.00");
+		
+		double burgerprice =1.69;
+		double friesPrice = 1.09;
+		double sodasprice = 0.99;
+		double amountTendered;
+		double change;
+		
+		int numburger;
+		int numfries;
+		int numsodas;
+		
+		System.out.println("Enter number of burgers.");
+		numburger= Scan.nextInt();
+		System.out.println("Enter number of Fries.");
+		numfries= Scan.nextInt();
+		System.out.println("Enter number of Sodas.");
+		numsodas= Scan.nextInt();
+		
+		
+		System.out.println("Total before tax $:" + df.format (1.69*numburger +1.09*numfries + 0.99*numsodas));
+		double total = (1.69*numburger +1.09*numfries + 0.99*numsodas);
+		System.out.println("Tax; $" + df.format (total*0.065));
+		double Tax = (1.69*numburger +1.09*numfries + 0.99*numsodas) * (0.065);
+		System.out.println("Final total: " + df.format (total +Tax));
+		double Finaltotal = (total +Tax);
+		
+		System.out.println("Enter tendered:");
+		amountTendered = Scan.nextInt();
+		
+		System.out.println("Change : $" + df.format (amountTendered - Finaltotal));
+		
+				
+		
+	
+		
+		
+		
+		
+			
+	}
+
+}
