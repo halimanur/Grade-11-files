4ff85ad74acca069f175c81fd993ac836a9154bd
333524585 <333524585@C-4028YR1.tdsb.on.ca>
2016-11-09 13:26:24 -0500

    Random 1 program

---
+++ ics3u/src/nur/Random1.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package nur;
+
+import java.util.Scanner;
+
+/**
+ * Halima Nur
+ * November 9 2016
+ * This program asks for the persons name, 70% of the time and their favorite name.
+ * Random.java
+ * 
+ */
+public class Random1 {
+
+
+
+	/**
+	 * @param args
+	
+	 */public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		String name;
+		double percentage;
+		
+		System.out.println("Enter your Name:");
+		name= scan.nextLine();
+		
+		percentage = Math.random();
+		
+		if (percentage>=0.7){
+			System.out.println("This is not your favorite name.");
+			
+		}
+		else {
+			System.out.println("This is your favorite name");
+			
+		
+		
+
+
+		
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
