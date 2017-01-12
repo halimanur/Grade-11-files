0e5581edf3854bd939e645088dae7bdc48f15c85
333524585 <333524585@C-DNY5WR1.tdsb.on.ca>
2017-01-12 13:25:44 -0500

    My letters

---
+++ ics3u/src/nur/Mississippi.java
@@ -0,0 +1,85 @@
+/**
+ * 
+ */
+package nur;
+
+/**
+ *Mississippi.java
+ * Halima Nur		
+ *January 11th 2017
+ *Prints out the word Mississippi
+ *
+ *
+ */
+	public class Mississippi 
+	
+	
+	
+	
+	{
+		public static void main(String[] args) {
+			m();
+			i();
+			s();
+			s();
+			i();
+			s();
+			s();
+			i();
+			p();
+			p();
+			i();
+		}	
+		/**
+		 * draws the letter M
+		 */
+			public static void m(){
+				
+		System.out.println("M     M");
+		System.out.println("MM   MM");
+		System.out.println("M M M M");
+		System.out.println("M  M  M");
+		System.out.println("M     M");
+		System.out.println("M     M");
+		System.out.println("M     M");
+		}
+			/**
+			 * draws the letter I
+			 */
+			public static void i(){
+			System.out.println("IIIII");
+			System.out.println("  I   ");
+			System.out.println("  I   ");
+			System.out.println("  I   ");
+			System.out.println("  I   ");
+			System.out.println("  I   ");
+			System.out.println("IIIII");
+			}
+			/**
+			 * draws the letter S
+			 */
+			public static void s(){
+		System.out.println(" SSSSS");
+		System.out.println("S     S");
+		System.out.println("S		");
+		System.out.println(" SSSSS");
+		System.out.println("      S");
+		System.out.println("S     S");
+		System.out.println(" SSSSS");
+			}
+
+			/**
+			 * draws the letter P
+			 */
+			public static void p(){
+			System.out.println("PPPPPPPP");		
+			System.out.println("PP    PP");
+			System.out.println("PP   PPP");
+			System.out.println("PPPPPPP ");
+			System.out.println("PP  ");
+			System.out.println("PP  ");
+			System.out.println("PP  ");
+			
+			}
+	}
+	
