55aaa4f7b85e06d1e5e86b5933bc998be5858c3c
333524585 <333524585@C-DNY5WR1.tdsb.on.ca>
2017-01-18 12:13:17 -0500

    My happy and you know it program

---
+++ ics3u/src/nur/HappyAndYouKnowIt.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package nur;
+
+import java.util.Scanner;
+	/**
+	 *Halima Nur
+	 *HappyAndYouKnowIt.java
+	 *Jan 18th 2017
+	 *A program that sings the song happy and you know it.
+	 */
+
+	public class HappyAndYouKnowIt {
+
+	/**
+	* @param args
+	*/
+	public static void main(String[] args) {
+
+
+	String [] lines = new String [] {"clap your hands", "stomp your feet", "snap you fingers", "shake around", "touch your nose", "put right hand up", "put your left hand up","put your right foot in", "put your left foot in", "close your eyes"}; 
+
+	for (int i=0; i!=10; i++)
+	{
+	System.out.println("If your happy and you know it " + lines [i]);
+	System.out.println("If your happy and you know it " + lines [i]);
+	System.out.println("If your happy and you know it and you really want to show it " + lines[i]);
+	System.out.println("If your happy and you know it " + lines[i]);
+	System.out.println(" ");
+	}
+	}
+
+	}
+
